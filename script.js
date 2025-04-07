3e6bd4741b006ac7806ee96bd324f4a3c495bbe0
MauricioScheffer <133931436+MauricioScheffer@users.noreply.github.com>
2025-04-07 15:22:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+// gsap
+// 	.timeline()
+// 	.to(".loader__logo", {
+// 		clipPath: "inset(0 0% 0 0)",
+// 		duration: 1,
+// 		ease: "power2.inOut"
+// 	})
+// 	.to(".loader svg path", {
+// 		y: 0,
+// 		stagger: .1,
+// 		duration: 2,
+// 		ease: "power2.inOut"
+// 	});
+
+const tl = gsap.timeline().to(".loader__logo", {
+	clipPath: "inset(0 0% 0 0)",
+	duration: 1,
+	ease: "power2.inOut"
+});
+const svgs = document.querySelectorAll(".loader svg");
+svgs.forEach((svg, i) => {
+	tl.to(
+		svg.querySelectorAll("path"),
+		{
+			y: 0,
+			duration: 0.8,
+			ease: "power2.out"
+		},
+		i == 0 ? "-=0.2" : "-=0.6"
+	);
+});
 
